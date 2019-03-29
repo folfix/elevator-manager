7bba8bfe82e49ad0ab219b2fc9cc680bc4042a5a
Jan Folfas <folfix@gmail.com>
2019-03-29 22:02:24 +0100

    Added first test

--- tests/elevator_test.cpp
+++ tests/elevator_test.cpp
@@ -1,5 +1,11 @@
 #include "gtest/gtest.h"
+#include "elevator.h"
 
 TEST(basic_test, eq) {
-    EXPECT_EQ(1, 0);
+    QApplication a();
+    auto *slider = new QSlider();
+    auto * elevator = new Elevator(QString("test"), 0, 10, slider);
+
+    EXPECT_EQ(elevator->getMinFloor(), 0);
+    EXPECT_EQ(elevator->getMaxFloor(), 10);
 }
